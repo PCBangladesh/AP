436dde87b6669a008afbc8c80e902d337c5ddf8c
PCBangladesh <paul7414@hotmail.com>
2022-07-22 23:07:26 -0500

    Project creation

---
+++ AP/AP.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AP.API.Startup))]
+
+namespace AP.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ AP/AP.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AP.Backend.MvcApplication" Language="C#" %>
